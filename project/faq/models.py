538ce2bf5e7a5d90628076fe3dc4f42762cc7eec
daniboyrc <danielrcifpb@gmail.com>
2017-11-05 15:33:56 -0300

    Comentado o arquivo models.py

--- project/faq/models.py
+++ project/faq/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.utils import timezone
 
 class Guardian(models.Model):
+    '''Classe que mapeia a tabela Guardian no banco de dados'''
+
     STATUS_CHOICES = (
         ("ativo", "ativo"),
         ("desativo", "desativo"),
@@ -16,9 +18,15 @@ class Guardian(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="ativo")
 
     def __str__(self):
+        ''' 
+        Retorna string que representa um Guardian
+        Segue o fomato: LOGIN_GUARDIAN
+        '''
         return self.login
 
 class FAQ(models.Model):
+    '''Classe que mapeia a tabela FAQ no banco de dados'''
+
     id_guardian = models.ForeignKey(Guardian, on_delete=models.CASCADE)
     data = models.DateField(blank=True, null=True)
     problema = models.CharField(max_length=255)
@@ -27,13 +35,21 @@ class FAQ(models.Model):
     curtidas = models.IntegerField(blank=True, null=True)
 
     def set_data(self):
+        '''Funcao que altera a data da FAQ para a data atual'''
+        
         self.data = timezone.now()
         self.save()
 
     def __str__(self):
+        '''
+        Retorna string que representa uma FAQ
+        Segue o formato: LOGIN_GUARDIAN - ID_FAQ - PROBLEMA
+        '''
         return self.id_guardina.login + " - " + str(self.id) + " - " + str(self.problema)
 
 class Solucao(models.Model):
+    '''Classe que mapeia a tabela Solucao no banco de dados'''
+
     id_guardian = models.ForeignKey(Guardian, on_delete=models.CASCADE)
     id_faq = models.ForeignKey(FAQ, on_delete=models.CASCADE)
     data = models.DateField(blank=True, null=True)
@@ -41,34 +57,57 @@ class Solucao(models.Model):
     fontes = models.TextField(blank=True, null=True)
 
     def set_data(self):
+        '''Funcao que altera a data da Solucao para a data atual'''
+
+        self.data = timezone.now() 
         self.data = timezone.now()
         self.save()
 
     def __str__(self):
+        '''
+        Retorna string que representa uma Solucao
+        Segue o formato: LOGIN_GUARDIAN - ID_SOLUCAO - ID_FAQ
+        '''
         return self.id_guardian.login + " - " + str(self.id) + " - " + str(self.id_faq.id)
 
 class Pergunta(models.Model):
+    '''Classe que mapeia a tabela Pergunta no banco de dados'''
+
     id_guardian = models.ForeignKey(Guardian, on_delete=models.CASCADE)
     data = models.DateTimeField(blank=True, null=True)
     pergunta = models.CharField(max_length=255)
     descricao = models.TextField()
 
     def set_data(self):
+        '''Funcao que altera a data da Pergunta para a data atual'''
+       
         self.data = timezone.now()
         self.save()
 
     def __str__(self):
+        '''
+        Retorna string que representa uma Pergunta
+        Segue o formato: LOGIN_GUARDIAN - ID_PERGUNTA - PERGUNTA
+        '''
         return self.id_guardian.login + " - " + str(self.id) + " - " + self.pergunta
 
 class Resposta(models.Model):
+    '''Classe que mapeia a tabela Resposta no banco de dados'''    
+
     id_guardian = models.ForeignKey(Guardian, on_delete=models.CASCADE)
     id_pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
     date = models.DateTimeField(blank=True, null=True)
     resposta = models.TextField()
 
-    def set_data(self):
+    def set_data(self):        
+        '''Funcao que altera a data da Resposta para a data atual'''
+
         self.data = timezone.now()
         self.save()
 
     def __str__(self):
+        '''
+        Retorna string que representa uma Resposta 
+        Segue o formato: LOGIN_GUARDIAN - ID_RESPOSTA - ID_PERGUNTA 
+        '''   
         return self.id_guardian.login + " - " + str(self.id) + " - " + str(self.id_pergunta.id)
