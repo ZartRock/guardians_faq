5a5efecf33e76e521535e369b6e988a31937c047
daniboyrc <danielrcifpb@gmail.com>
2017-11-05 09:39:21 -0300

    Criada classe Guardian em models.py

--- project/faq/models.py
+++ project/faq/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Guardian(models.Model):
+    STATUS_CHOICES = (
+        ("ativo", "ativo"),
+        ("desativo", "desativo"),
+    )
+
+    login = models.CharField(max_length=45, unique=True)
+    senha = models.CharField(max_length=45)
+    nome = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="ativo")
+
+    def __str__(self):
+        return self.login
